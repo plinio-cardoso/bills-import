4c4161846051b9e24dfce89faceed4855947839a
Plinio Cardoso <plynyo@gmail.com>
2023-06-07 10:12:25 -0300

    Added categories

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bills-import
+
+## Setup
+
+- docker compose up -d
+- docker compose run --rm composer install
+- docker compose run --rm node npm install
+- docker compose run --rm node npm run dev
--- src/Service/Converter.php
+++ src/Service/Converter.php
@@ -8,6 +8,74 @@ class Converter
 {
     private ImportStatus $importStatus = ImportStatus::STOPPED;
 
+    private array $categoryMapping = [
+        'Refeição' => [
+            'iFood',
+            'Panificadora',
+            'Padoka',
+            'CONVENIENCIA 855',
+            'Carnes',
+            'SORVETERIA',
+            'PIZZARIA',
+            'HAMBURGUERIA',
+            'MR DOOGS',
+            'QUE TEMPERO',
+            'RESTAURANT',
+            'ACOUGUE',
+            'DOM ROCHA',
+            'Flechazo',
+            'LANCHONETE',
+            'LUMBERJACK',
+        ],
+        'Transporte' => [
+            'Posto',
+            'PRADO MULTIMARCAS',
+            'TIRESHOP',
+            'RENATO AUTO SERVICE'
+        ],
+        'Compras Online' => [
+            'MercadoLivre',
+            'AMAZON',
+            'APPLE COM BILL',
+            'Adidas',
+            'Nike',
+        ],
+        'Beleza' => [
+            'BARBEARIA',
+        ],
+        'Saúde' => [
+            'FARMACIA',
+            'RAIA',
+        ],
+        'Esporte' => [
+            'BT FO LONDRINA',
+        ],
+        'Lazer' => [
+            'Netflix',
+        ],
+        'Supermercado' => [
+            'MUFFATO',
+            'SUPERMERCADOS',
+        ],
+        'Telefone / Internet' => [
+            'VIVO'
+        ],
+        'PET' => [
+            'PetShop',
+            'PetCenterComercio',
+            'Petzaplicativo',
+        ],
+        'Diversos Plinio' => [
+            'CASA DOS PARAFUSOS',
+            'MACRIPAR',
+            'DUQUE COMPONENTES',
+            'ALELUZ',
+            'GranaCapital',
+            'Suno Research',
+            'PetCenterComercio',
+        ]
+    ];
+
     public function convertBradesco($path): string
     {
         $data = file_get_contents($path);
@@ -34,14 +102,28 @@ class Converter
         return implode("\n", $csv);
     }
 
+    function mapCategoryName($name): string
+    {
+        foreach ($this->categoryMapping as $categoryName => $keywords) {
+            foreach ($keywords as $keyword) {
+                if (str_contains(strtolower($name), strtolower($keyword)) !== false) {
+                    return $categoryName;
+                }
+            }
+        }
+
+        return 'PREENCHER';
+    }
+
     private function buildCsvLine($line): string
     {
+        $name = trim(str_replace('/', '|', $line[1]));
         $items = [
             sprintf('%s/%s', $line[0], date('Y')),
-            str_replace('/', '|', $line[1]),
+            $name,
             '-' . str_replace(',', '.', $line[3]),
             'Bradesco',
-            'PREENCHER'
+            $this->mapCategoryName($name)
         ];
 
         return implode(',', $items);
